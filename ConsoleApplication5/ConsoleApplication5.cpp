797bc7fb22c49a6b54871a51b65b157833fe24d1
Student <Student@S215-1>
2021-01-29 14:21:46 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,31 @@
+﻿#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Ru");
+	int m, n;
+	cout << "Число m" << endl;;
+	cin >> m;
+	cout << "Число n" << endl;
+	cin >> n;
+	if (m != n)
+	{
+		if (m > n)
+		{
+			m = n;
+		}
+		if (n > m)
+		{
+			n = m;
+		}
+	}
+	else
+	{
+		m = n;
+	}
+	cout << m << "";
+	cout << n << "";
+	return (0);
+}
